6caecd1b23c132715cbb9afce8b7b59d1ef1d01a
CDP360 <kalai@cdp360.com>
2022-11-26 23:35:31 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.scss";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Homepage/Home";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </Router>
+        </div>
+    );
+}
+
+export default App;
+
