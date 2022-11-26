204c307ef7d3ab98f40fa090f98e91839814bb3c
CDP360 <kalai@cdp360.com>
2022-11-27 00:51:08 +0530

    second

---
+++ src/components/Data/Data.js
@@ -0,0 +1,115 @@
+const datas = [
+    {
+        id: 1,
+        question: "what is u r favor?",
+        answeroptions: [
+            {
+                answertext: "first",
+                isCorrect: false,
+            },
+            {
+                answertext: "second",
+                isCorrect: false,
+            },
+            {
+                answertext: "third",
+                isCorrect: true,
+            },
+            {
+                answertext: "fourth",
+                isCorrect: false,
+            }
+        ]
+    },
+    {
+        id: 2,
+        question: "what is u r city?",
+        answeroptions: [
+            {
+                answertext: "chennai",
+                isCorrect: false,
+            },
+            {
+                answertext: "madurai",
+                isCorrect: false,
+            },
+            {
+                answertext: "krishnagiri",
+                isCorrect: true,
+            },
+            {
+                answertext: "mathur",
+                isCorrect: false,
+            }
+        ]
+    },
+    {
+        id: 3,
+        question: "what is u r app?",
+        answeroptions: [
+            {
+                answertext: "fb",
+                isCorrect: false,
+            },
+            {
+                answertext: "twitter",
+                isCorrect: false,
+            },
+            {
+                answertext: "whats app",
+                isCorrect: true,
+            },
+            {
+                answertext: "instagarm",
+                isCorrect: false,
+            }
+        ]
+    },
+    {
+        id: 4,
+        question: "what is u r bike?",
+        answeroptions: [
+            {
+                answertext: "Ns 200",
+                isCorrect: true,
+            },
+            {
+                answertext: "FZ",
+                isCorrect: false,
+            },
+            {
+                answertext: "R15",
+                isCorrect: true,
+            },
+            {
+                answertext: "KTM",
+                isCorrect: false,
+            }
+        ]
+    },
+    {
+        id: 5,
+        question: "what is u r juice?",
+        answeroptions: [
+            {
+                answertext: "mango",
+                isCorrect: false,
+            },
+            {
+                answertext: "orange",
+                isCorrect: false,
+            },
+            {
+                answertext: "lemon",
+                isCorrect: true,
+            },
+            {
+                answertext: "soda",
+                isCorrect: false,
+            }
+        ]
+    },
+
+]
+
+export default datas;
--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -1,8 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import datas from './../Data/Data';
+import './styles/Home.scss';
 
 function Home() {
+
+
+    const [data, setData] = useState(datas);
+
+    const [show, setShow] = useState(false);
+
+    const [step, setStep] = useState(0);
+
+    const [sources, setSources] = useState(0);
+
+    const [emojes, setEmojes] = useState(``)
+    const [emojes2, setEmojes2] = useState(``)
+    const [emojes3, setEmojes3] = useState(``)
+    const [emojes4, setEmojes4] = useState(``)
+    const [emojes5, setEmojes5] = useState(``)
+
+
+
+
+
+    useEffect(() => {
+        setData(datas);
+
+        if (sources === 0) {
+            setEmojes(`😭`);
+        }
+        if (sources === 1) {
+            setEmojes2(`🥲`)
+        }
+        if (sources === 2) {
+            setEmojes3(`☹️`)
+        }
+        if (sources === 3) {
+            setEmojes4(`😁`)
+        }
+        if (sources === 4) {
+            setEmojes5(`😎`)
+        }
+    }, [])
+
+    const handlemove = (correct) => {
+
+        if (correct === true) {
+            setSources(sources + 1);
+        }
+
+        const nextstepcount = step + 1;
+        if (nextstepcount < data.length) {
+            setStep(nextstepcount);
+        }
+        else {
+            setShow(true);
+        }
+    }
+
+    const ReStart = () => {
+        setSources(0);
+        setShow(false);
+        setStep(0);
+    }
+
+
     return (
-        <div>Home</div>
+        <div className='main-section'>
+
+            <div className='inside-section'>
+                {data.length === 0 && <div>No Data Found</div>}
+
+                <div className='text-center mb- fs-1'>
+                    <h1>{data[step]?.question}</h1>
+
+                </div>
+                {show ? <>
+                    <div className='text-center mb- fs-1'>
+
+                        <div className='text-center'>
+                            {sources === 0 && <h1>😭</h1>}
+                            {sources === 1 && <h1>🥲</h1>}
+                            {sources === 2 && <h1>☹️</h1>}
+                            {sources === 3 && <h1>😁</h1>}
+                            {sources === 4 && <h1>😎</h1>}
+                            {sources === 5 && <h1>🤩</h1>}
+                        </div>
+                        <h1>question Correct {sources}/{data.length}  </h1>
+                        <div>
+                            <button onClick={ReStart}>Restart</button>
+                        </div>
+                    </div>
+                </> : <>
+
+                    <div>
+                        <h1>question Coorect {step + 1}/{data.length} </h1>
+                    </div>
+                    {data[step]?.answeroptions?.map((items, index) => {
+                        return (
+                            <div key={index}>
+                                <button className='buttons mt-2 ' onClick={() => handlemove(items?.isCorrect)}>{items?.answertext}</button>
+                            </div>
+                        )
+                    })}
+                </>}
+            </div>
+        </div>
     )
 }
 
